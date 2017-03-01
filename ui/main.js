54e6230af6048b8aec0916672b5fae10b85fbb24
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-01 15:12:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+var button=document.getElementById('count');
+button.onclick = function(){
+    var req= new XMLHttpRequest();
+    req.onreadystagechange=function(){
+        if(req.readyState==XMLHttpRequest.DONE){
+            if(req.status==200){
+                var counter=req.responseText;
+                var span=document.getElementById('cnt');//chg
+                span.innerHTML=counter.toString();//chg
+            }
+        }
+        
+    };
+    
+    req.open('GET','http://dineshparthi.imad.hasura-app.io/counter',true);
+    req.send(null);
+}
