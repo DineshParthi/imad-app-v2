15227878f502a9d7a7c2433d291083b578bf911d
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-02 20:29:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,10 @@ var button1=document.getElementById('counter');
     request.onreadystagechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             alert("h2");
+            console.log(`Hi2`);
             if(request.status === 200){
                 alert("h1");
+                console.log(`Hi3`);
                 var counter=request.responseText;
                 var span=document.getElementById('cnt');//chg
                 span.innerHTML=counter.toString();//chg
