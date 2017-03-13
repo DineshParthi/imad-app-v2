2fa77bde26902d4afe8763658a2b457080a7a924
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-13 19:35:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ var submit_btn= document.getElementById('submit_btn');
     var names=['name1','name2','name3','name4'];
     var list="";
     for(i=0;i< names.length;i++){
-        list='<li>'+names[i]+'</li>';
+        list+='<li>'+names[i]+'</li>';
     }
     alert(list);
     var ol=document.getElementById('namelist');
