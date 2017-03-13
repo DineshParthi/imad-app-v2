8634c6cc4f0fc4b66db8cb53d25722849680d324
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-13 20:03:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names[];
+app.get('/submitname', function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/art1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
 });
