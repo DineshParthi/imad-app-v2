633caad3ffff5d6c4ac2daca955bf87d1150a904
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-13 20:14:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,19 +25,37 @@ var button1=document.getElementById('counter');
 }
 
 //submit name
-var nameInput= document.getElementById('nameInput');
-//var name=nameInput.value;
+
 var submit_btn= document.getElementById('submit_btn');
 //submit_btn.onclick= function(){
  
- function listme() {  
-    var names=['name1','name2','name3','name4'];
-    var list="";
-    //for(i=0;i< names.length;i++){
-    for(i in names){
-        list+='<li>'+names[i]+'</li>';
-    }
-    alert(list);
-    var ol=document.getElementById('namelist');
-    ol.innerHTML=list;
+ function listme() {
+     var request= new XMLHttpRequest();
+    console.log(`Hi5`);
+    request.onreadystatechange = function(){
+        console.log(`Hi3`);
+        if(request.readyState === XMLHttpRequest.DONE){
+            console.log(`Hi2`);
+            if(request.status === 200){
+                console.log(`Hi3`);
+                 //res
+                var names=['name1','name2','name3','name4'];
+                var list="";
+                //for(i=0;i< names.length;i++){
+                for(i in names){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                //alert(list);
+                var ol=document.getElementById('namelist');
+                ol.innerHTML=list;
+            }
+        }
+        console.log(request.readyState);
+        
+    };
+    var nameInput= document.getElementById('nameInput');
+    var name=nameInput.value;
+    request.open('GET','http://dineshparthi.imad.hasura-app.io/submitname?name='+name,true);
+    request.send(null);
+    
 };
