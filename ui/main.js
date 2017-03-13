6b620f1763ed8b520c1393976b440c2363f51669
DineshParthi <dinesh.parthiban92@gmail.com>
2017-03-13 19:41:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ var submit_btn= document.getElementById('submit_btn');
     var names=['name1','name2','name3','name4'];
     var list="";
     //for(i=0;i< names.length;i++){
-    for(i in names)
+    for(i in names){
         list+='<li>'+names[i]+'</li>';
     }
     alert(list);
